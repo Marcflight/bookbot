5789ac416a41966c4e2bdbb6bb6a373b9a344a58
marcflight <markus.lovenjak@web.de>
2025-01-02 14:58:26 +0100

    v1.0

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_word_count(text)
+    num_words = get_num_words(text)
     character_count = get_character_count(text)
-    print(character_count)
+    chars_sorted_list = chars_dict_to_sorted_list(character_count)
 
-#Calculating the word count
-def get_word_count(text):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
     words = text.split()
     return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 #Accessing Book text
 def get_book_text(path):
@@ -25,8 +48,5 @@ def get_character_count(text):
         else:
             characters[lowered] = 1
     return characters
-
-
-
 
 main()
